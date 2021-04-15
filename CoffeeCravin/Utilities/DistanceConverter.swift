2981bce0bf7e7940df5288b2f25a592a43fb8c44
GeorgeCremer <george.cremer@gmail.com>
2021-04-15 21:05:43 +0100

    improved DistanceFormatter

--- CoffeeCravin/Utilities/DistanceConverter.swift
+++ CoffeeCravin/Utilities/DistanceConverter.swift
@@ -9,17 +9,11 @@ import Foundation
 
 struct DistanceFormatter {
     // MARK: - DistanceConversion
-
-    private let numberFormatter = NumberFormatter()
-
     func convert(distance: Measurement<UnitLength>) -> String {
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.locale = Locale.current
-
         let distanceInMeters = distance.converted(to: .meters).value.rounded()
         if distanceInMeters >= 1000 {
             let km = distance.converted(to: .kilometers).value.rounded(toPlaces: 1)
-            if km >= 10 {
+            if km <= 10 {
                 return "\(km)km"
             } else {
                 return "\(Int(km))km"
